effa59d0bd06848f9395c51ea60be3734119aed5
yangxin <xinxiny@aliyun.com>
2019-10-28 20:14:19 +0800

    server.port

--- config-server/src/test/java/link/yangxin/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/link/yangxin/configserver/ConfigServerApplicationTests.java
@@ -1,11 +1,27 @@
 package link.yangxin.configserver;
 
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
-class ConfigServerApplicationTests {
+@RunWith(SpringRunner.class)
+public class ConfigServerApplicationTests {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Test
+    public void test1(){
+        //stringRedisTemplate.opsForValue().set("name", "杨鑫");
+        //stringRedisTemplate.opsForValue().set("age", "25");
+        System.out.println(stringRedisTemplate.opsForValue().get("name"));
+
+    }
 
 }
